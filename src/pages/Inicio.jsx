5a65b1d05b9af8c1227382bf84fa14e609b9b27d
Caio Lima <caiojlimah@gmail.com>
2021-12-14 17:48:59 -0300

    Iniciando o Portifólio

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Inicio.css';
+
+const Inicio = () => (
+  <div>
+    <nav>
+      <ul>
+        <li>Início</li>
+        <li>Sobre</li>
+        <li>Projetos</li>
+        <li>Contato</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Inicio;

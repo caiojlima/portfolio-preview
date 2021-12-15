09e4da6a2b59ae25dce1c017f18ab35a2924899b
Caio Lima <caiojlimah@gmail.com>
2021-12-14 22:49:39 -0300

    Page contato feita

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Nav.css';
 
 const Nav = () => (
   <nav>
     <ul>
       <Link to="/"><li>Início</li></Link>
       <Link to="/about"><li>Sobre</li></Link>
-      <Link to="projects"><li>Projetos</li></Link>
-      <Link to="contact"><li>Contato</li></Link>
+      <Link to="/projects"><li>Projetos</li></Link>
+      <Link to="/contact"><li>Contato</li></Link>
     </ul>
   </nav>
 );
--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import github from '../images/github.svg';
+import linkedin from '../images/linkedin.svg';
+import email from '../images/email.svg'
+import '../styles/Contato.css';
 
 const Contato = () => (
   <div>
     <Nav />
-    <h1>Aqui é a página de contato</h1>
+    <div className="title-container">
+      <h1>Contato</h1>
+    </div>
+    <section className="links-container">
+      <div>
+        <a href="https://github.com/caiojlima" target="_blank" rel="noreferrer">
+          <img src={ github } alt="Link para Github" />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/caio-limah/" target="_blank" rel="noreferrer">
+          <img src={ linkedin } alt="Link para Linkedin" />
+        </a>
+      </div>
+    </section>
+    <section className="links-container email-section">
+    <div>
+        <a href="mailto:caiojlimah@gmail.com" target="_blank" rel="noreferrer">
+          <img src={ email } alt="Link para Mensagem de Email" />
+        </a>
+      </div>
+    </section>
   </div>
 );
 

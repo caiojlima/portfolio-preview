53acc84a1b64944fe614c1274f155c4ed7dd202e
Caio Lima <caiojlimah@gmail.com>
2021-12-16 03:51:32 -0300

    Ajustando CSS página Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import '../styles/Sobre.css';
 
 const Sobre = () => (
   <div>
     <Nav />
-    <h1>Aqui é a página de Sobre</h1>
+    <div className="title-container">
+      <h1>Página em construção<span className="blink-1">.</span><span className="blink-2">.</span><span className="blink-3">.</span></h1>
+    </div>
   </div>
 );
 

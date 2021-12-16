367625646e9fe37736bf68f86b53a562a2425465
Caio Lima <caiojlimah@gmail.com>
2021-12-16 16:31:07 -0300

    Ajustando NotFoundPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Nav from '../components/Nav';
-import notFound from '../images/notFound.svg';
+import notFound from '../images/notFoundOct.svg';
 import '../styles/NotFound.css';
 
 const NotFound = () => (
   <div>
     <Nav />
     <div className="not-found-container">
+      <div className="error-message-container">
+        <h2>ERROR 404.</h2>
+        <p>Página não encontrada.</p>
+      </div>
       <img src={ notFound } alt="Oops! Página não encontrada!" />
     </div>
   </div>
--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import aplication from '../images/aplication.gif';
+import trivia from '../images/trivia.gif';
 import ProjectCard from '../components/ProjectCard';
 import '../styles/Projetos.css';
 
@@ -15,7 +16,14 @@ const Projetos = () => (
         <ProjectCard
         name="Front-End Online store"
         img={ aplication }
-        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API"
+        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API."
+        />
+      </a>
+      <a href="https://github.com/caiojlima/trivia" target="_blank" rel="noreferrer">
+        <ProjectCard
+        name="Trivia Trybe"
+        img={ trivia }
+        description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
         />
       </a>
     </div>

622e10a0bd27ee92c551622b8e513edb0bc86be1
Caio Lima <caiojlimah@gmail.com>
2021-12-17 04:50:25 -0300

    Ajustando página projetos

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -26,62 +26,6 @@ const Projetos = () => (
         description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
         />
       </a>
-      <a href="https://github.com/caiojlima/online-store" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Front-End Online store"
-        img={ aplication }
-        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/trivia" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Trivia Trybe"
-        img={ trivia }
-        description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/online-store" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Front-End Online store"
-        img={ aplication }
-        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/trivia" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Trivia Trybe"
-        img={ trivia }
-        description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/online-store" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Front-End Online store"
-        img={ aplication }
-        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/trivia" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Trivia Trybe"
-        img={ trivia }
-        description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/online-store" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Front-End Online store"
-        img={ aplication }
-        description="Projeto desenvolvido durante o curso da Trybe. Nele eu consegui me aprofundar no React, no RTL e também nas requisições de API."
-        />
-      </a>
-      <a href="https://github.com/caiojlima/trivia" target="_blank" rel="noreferrer">
-        <ProjectCard
-        name="Trivia Trybe"
-        img={ trivia }
-        description="Projeto em grupo da Trybe onde pude evoluir minhas soft skills enquanto aprendia a utilizar o Redux e o Redux Thunk."
-        />
-      </a>
     </div>
   </div>
 );
